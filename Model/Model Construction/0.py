9070e85b56f7cced1c32b13c74c1c11606fcc36a
HELEN Ting He <32111795+tinghe14@users.noreply.github.com>
2023-06-05 13:34:29 -0400

    Update 0.py

--- Model/Model Construction/0.py
+++ Model/Model Construction/0.py
@@ -1,35 +1,9 @@
+# REF: https://debuggercafe.com/saving-and-loading-the-best-model-in-pytorch/
+# good tutorial contains how to save the best model based on validation loss =(apply early stopping)
+# very stuctured scripts
+
 # BACKGROUND ------
-def build_model(pretrained=True, fine_tune=True, num_classes=1):
-    """
-    Function to build the neural network model. Returns the final model.
-    Parameters
-    :param pretrained (bool): Whether to load the pre-trained weights or not.
-    :param fine_tune (bool): Whether to train the hidden layers or not.
-    :param num_classes (int): Number of classes in the dataset. 
-    """
-    if pretrained:
-        print('[INFO]: Loading pre-trained weights')
-    elif not pretrained:
-        print('[INFO]: Not loading pre-trained weights')
-    model = models.resnet18(pretrained=pretrained)
-    if fine_tune:
-        print('[INFO]: Fine-tuning all layers...')
-        for params in model.parameters():
-            params.requires_grad = True
-    elif not fine_tune:
-        print('[INFO]: Freezing hidden layers...')
-        for params in model.parameters():
-            params.requires_grad = False
-            
-    # change the final classification head, it is trainable
-    model.fc = nn.Linear(512, num_classes)
-    return model
-  
-# build the model
-model = build_model(
-    pretrained=False, fine_tune=True, num_classes=10
-).to(device)
-print(model)
+# pass
 
 # CORE --------
 # total parameters and trainable parameters
