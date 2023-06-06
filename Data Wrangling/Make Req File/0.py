61768a95e5cd9a65bbd757addd676725803607ed
HELEN Ting He <32111795+tinghe14@users.noreply.github.com>
2023-06-06 15:38:03 -0400

    Create 0.py

---
+++ Data Wrangling/Make Req File/0.py
@@ -0,0 +1,10 @@
+# reference: https://stackoverflow.com/questions/58737741/how-to-list-all-python-packages-used-by-a-script-in-python3?noredirect=1&lq=1
+
+# in the terminal 
+>> pip3 install pipreqs
+# DNK but pip won't work
+>> pwd 
+# get current directory
+>> pipreqs ./your_script_directory
+# the directory of your scripts
+>> INFO: Successfully saved requirements file in /home/project/location/requirements.txt
